82ece48a1254516dd9c8add5c5348683ff6932f8
izgarshev <izgarshev_p@mail.ru>
2021-11-30 00:56:53 +0300

    first version

--- src/main/java/ru/eleron/org/Converter.java
+++ src/main/java/ru/eleron/org/Converter.java
@@ -5,6 +5,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.wp.usermodel.HeaderFooterType;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -30,24 +31,19 @@ public class Converter {
         Set<CellType> types = new HashSet<>();
         try {
             workbook = (XSSFWorkbook) WorkbookFactory.create(file);
-            XSSFSheet sheet = workbook.getSheet("Лист1");
+            XSSFSheet sheet = workbook.getSheet("Лист2");
             Iterator<Row> rowIterator = sheet.rowIterator();
             int cellNumber;
-            try {
-//                XWPFParagraph paragraph = document.createParagraph();
-//                table.createRow().createCell().addParagraph().createRun().setText("text in the table");
-//                paragraph.createRun();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
             File docxFile = new File("C:\\Code\\tests.docx");
             file.createNewFile();
             XWPFDocument document = new XWPFDocument();
+            document.createHeader(HeaderFooterType.DEFAULT);
+
             while (rowIterator.hasNext()) {
                 XSSFRow row = (XSSFRow) rowIterator.next();
                 Iterator<Cell> cellIterator = row.cellIterator();
                 cellNumber = 1;
-                XWPFTable table = createTable(document, 7, 1);
+                XWPFTable table = createTable(document, 10, 1);
                 while (cellIterator.hasNext()) {
                     Cell next = cellIterator.next();
                     switch (cellNumber) {
@@ -56,23 +52,85 @@ public class Converter {
                             table.getCTTbl().newCursor().toEndToken();
                             XWPFParagraph xwpfParagraph = table.getRow(cellNumber - 1).getCell(0).addParagraph();
                             xwpfParagraph.setAlignment(ParagraphAlignment.CENTER);
-                            xwpfParagraph.createRun().setText(readValue(next));
+                            xwpfParagraph.createRun().setText("Тип теста: " + readValue(next).trim());
                             break;
                         case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 7:
+
                             XWPFParagraph xwpfParagraph2 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
                             xwpfParagraph2.setAlignment(ParagraphAlignment.CENTER);
-                            table.getRow(cellNumber - 1).getCell(0).addParagraph().createRun().setText(readValue(next));
+                            xwpfParagraph2.createRun().setText("Заголовок теста: " + readValue(next).trim());
+                            break;
+                        case 3:
+                            table.getCTTbl().newCursor().toEndToken();
+                            XWPFParagraph xwpfParagraph3 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph3.setAlignment(ParagraphAlignment.CENTER);
+                            XWPFRun run = xwpfParagraph3.createRun();
+                            run.setBold(true);
+                            run.setText("Вопрос: " + readValue(next).trim());
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            String value1 = readValue(next);
+                            if (value1.equals("'не задано'")) {
+                                break;
+                            }
+                            table.getCTTbl().newCursor().toFirstContentToken();
+                            XWPFParagraph xwpfParagraph5 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph5.setAlignment(ParagraphAlignment.CENTER);
+                            xwpfParagraph5.createRun().setText("Ответ 1: " + readValue(next).trim());
+                            break;
+                        case 6:
+                            String value2 = readValue(next);
+                            if (value2.equals("'не задано'")) {
+                                break;
+                            }
+                            XWPFParagraph xwpfParagraph6 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph6.setAlignment(ParagraphAlignment.CENTER);
+                            xwpfParagraph6.createRun().setText("Ответ 2: " + readValue(next).trim());
+                            break;
+                        case 7:
+                            String value3 = readValue(next);
+                            if (value3.equals("'не задано'")) {
+                                break;
+                            }
+                            XWPFParagraph xwpfParagraph7 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph7.setAlignment(ParagraphAlignment.CENTER);
+                            xwpfParagraph7.createRun().setText("Ответ 3: " + readValue(next).trim());
+                            break;
+                        case 8:
+                            String value4 = readValue(next);
+                            if (value4.equals("'не задано'")) {
+                                break;
+                            }
+                            XWPFParagraph xwpfParagraph8 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph8.setAlignment(ParagraphAlignment.CENTER);
+                            xwpfParagraph8.createRun().setText("Ответ 4: " + readValue(next).trim());
+                            break;
+                        case 9:
+                            String value5 = readValue(next);
+                            if (value5.equals("'не задано'")) {
+                                break;
+                            }
+                            XWPFParagraph xwpfParagraph9 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph9.setAlignment(ParagraphAlignment.CENTER);
+                            xwpfParagraph9.createRun().setText("Ответ 5: " + readValue(next).trim());
+                            break;
+                        case 10:
+                            String value6 = readValue(next);
+                            if (value6.equals("'не задано'")) {
+                                break;
+                            }
+                            XWPFParagraph xwpfParagraph10 = table.getRow(cellNumber - 1).getCell(0).addParagraph();
+                            xwpfParagraph10.setAlignment(ParagraphAlignment.CENTER);
+                            table.getRow(cellNumber - 1).getCell(0).addParagraph().createRun().setText("Ответ 6: " + readValue(next));
                             break;
                     }
                     types.add(next.getCellTypeEnum());
                     cellNumber++;
                 }
             }
+
             FileOutputStream fileOutputStream = new FileOutputStream(docxFile);
             document.write(fileOutputStream);
             fileOutputStream.flush();
